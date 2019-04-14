9be9b647a369f71e5adcbe49a87d857233a5358c
Hyuni89 <cuhcuh1@naver.com>
2019-04-14 19:51:29 +0900

    update

--- install/vim.sh
+++ install/vim.sh
@@ -3,4 +3,4 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # vim colorscheme
 mkdir -p ~/.vim/colors
-curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim ~/.vim/colors
+curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim && mv jellybeans.vim ~/.vim/colors
